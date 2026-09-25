915d7b9c378d1948526bf5b99b8460e225951e91
Warpointpro <joniii0550@gmail.com>
2026-09-24 21:17:10 -0300

    ACTU

--- clase 5/tarea4/ejercicio2.java
+++ clase 5/tarea4/ejercicio2.java
@@ -1,3 +1,29 @@
-public class ejercicio2 {
-    
+public class ejercicio2{
+
+//EMPEZAMOS CREANDO CLASE VEHICULO CON ATRIBUTOS COMO MARCA, MODELO Y PRECIO//
+   public class Vehiculo {
+    private String marca;
+    private String modelo;
+    private double precio;
+
+    // Constructor
+    public Vehiculo(String marca, String modelo, double precio) {
+        this.marca = marca;
+        this.modelo = modelo;
+        this.precio = precio;
+    }
+
+    // Getters
+    public String getMarca() {
+        return marca;
+    }
+
+    public String getModelo() {
+        return modelo;
+    }
+
+    public double getPrecio() {
+        return precio;
+     }
+  }
 }
